879efc6f4247324df24a93af6599f9dc118cc8ca
AndrewRFrench <98298580+andrewrfrench@users.noreply.github.com>
2024-09-21 20:11:30 -0500

    Update wkt2.py

--- wkt2.py
+++ wkt2.py
@@ -6,11 +6,11 @@ import streamlit as st
 st.title('Try Your Hand At WKT!')
 st.write('WKT means Well-Known Text and represents vector geometry in an easy to understand format. Below are some samples of WKT to try!')
 
-st.write("POINT (30 10)\n
-	LINESTRING (30 10, 10 30, 40 40)  \n
-	POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))  \n
-	POLYGON ((35 10, 45 45, 15 40, 10 20, 35 10),  \n
-	(20 30, 35 35, 30 20, 20 30))")
+st.write("""POINT (30 10)\n
+	LINESTRING (30 10, 10 30, 40 40)
+	POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))
+	POLYGON ((35 10, 45 45, 15 40, 10 20, 35 10), 
+	(20 30, 35 35, 30 20, 20 30))""")
 
 user = st.text_input("Enter WKT Below", value='POLYGON ((40 40, 20 45, 45 30, 40 40))')
 
