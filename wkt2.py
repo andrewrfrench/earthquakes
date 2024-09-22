9ec77eab8735e7407310da308e2a0781dd446e92
AndrewRFrench <98298580+andrewrfrench@users.noreply.github.com>
2024-09-21 20:10:30 -0500

    Update wkt2.py

--- wkt2.py
+++ wkt2.py
@@ -10,7 +10,7 @@ st.write("POINT (30 10)\n
 	LINESTRING (30 10, 10 30, 40 40)  \n
 	POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))  \n
 	POLYGON ((35 10, 45 45, 15 40, 10 20, 35 10),  \n
-	(20 30, 35 35, 30 20, 20 30))')"
+	(20 30, 35 35, 30 20, 20 30))")
 
 user = st.text_input("Enter WKT Below", value='POLYGON ((40 40, 20 45, 45 30, 40 40))')
 
