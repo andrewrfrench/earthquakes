e332fb4c933c638e872e18e66dec8f1f2ccdee6e
AndrewRFrench <98298580+andrewrfrench@users.noreply.github.com>
2025-06-11 11:21:13 -0500

    Update federal_debt.py

--- federal_debt.py
+++ federal_debt.py
@@ -65,8 +65,7 @@ with col2:
     fig2 = px.bar(new_df, x='date', y='deficit', color='president',
                   labels={
                          "date": "Date",
-                         "ratio": "Surplus or \n
-                         Deficit/GDP"
+                         "ratio": "Surplus or Deficit/GDP"
                      },
                     title="Federal Surplus or Deficit as % of GDP")
     
